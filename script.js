69acbb4f9ae44774d0b7f1f5823f4d97120de42e
zolon wilkins <zolonsk8@gmail.com>
2016-07-10 16:51:06 -0500

    added text effects to overlay headers

--- script.js
+++ script.js
@@ -6,6 +6,45 @@ $(function(){
       $('.bot-overlay').addClass('hidden')
     }
   )
+
+  $('#bot').hover(
+    function(){
+      $('#bot').effect( "shake", { direction: "right", times: 4, distance: 10}, 1000 )
+    },
+    function(){
+      $('#bot').complete('shake')
+
+    }
+  )
+  $('#ud').hover(
+    function(){
+      $('#ud').effect( "shake", { direction: "right", times: 4, distance: 10}, 1000 )
+    },
+    function(){
+      $('#ud').complete('shake')
+
+    }
+  )
+
+  $('#uc').hover(
+    function(){
+      $('#uc').effect( "shake", { direction: "up", times: 4, distance: 10}, 1000 )
+    },
+    function(){
+      $('#uc').complete('shake')
+
+    }
+  )
+  $('#dt').hover(
+    function(){
+      $('#dt').effect( "shake", { direction: "up", times: 4, distance: 10}, 1000 )
+    },
+    function(){
+      $('#dt').complete('shake')
+
+    }
+  )
+
   $('.updog').hover(
     function() {
       $('.dog-overlay').removeClass('hidden')
