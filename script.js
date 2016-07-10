7abcaa2821e118be3fff0124bbb32566b4826b27
zolon wilkins <zolonsk8@gmail.com>
2016-07-09 23:04:10 -0500

    new projects view

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 $(function(){
+  $('.datbot').hover(
+    function() {
+      $('.bot-overlay').removeClass('hidden')
+    }, function () {
+      $('.bot-overlay').addClass('hidden')
+    }
+  )
+  $('.updog').hover(
+    function() {
+      $('.dog-overlay').removeClass('hidden')
+    }, function () {
+      $('.dog-overlay').addClass('hidden')
+    }
+  )
+  $('.daytrippr').hover(
+    function() {
+      $('.trip-overlay').removeClass('hidden')
+    }, function () {
+      $('.trip-overlay').addClass('hidden')
+    }
+  )
+  $('.ubercart').hover(
+    function() {
+      $('.cart-overlay').removeClass('hidden')
+    }, function () {
+      $('.cart-overlay').addClass('hidden')
+    }
+  )
+
+
 
 
 })
