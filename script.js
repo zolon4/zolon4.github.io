40ea06411dab3c4b43dc039a1e3e9e1b623d3c5e
zolon wilkins <zolonsk8@gmail.com>
2016-07-10 19:37:50 -0500

    added new overlay animations

--- script.js
+++ script.js
@@ -2,68 +2,48 @@ $(function(){
   $('.datbot').hover(
     function() {
       $('.bot-overlay').removeClass('hidden')
+      $('#bot').addClass('bounce')
+
     }, function () {
       $('.bot-overlay').addClass('hidden')
-    }
-  )
-
-  $('#bot').hover(
-    function(){
-      $('#bot').effect( "shake", { direction: "right", times: 4, distance: 10}, 1000 )
-    },
-    function(){
-      $('#bot').complete('shake')
-
-    }
-  )
-  $('#ud').hover(
-    function(){
-      $('#ud').effect( "shake", { direction: "right", times: 4, distance: 10}, 1000 )
-    },
-    function(){
-      $('#ud').complete('shake')
+      $('#bot').removeClass('bounce')
 
     }
   )
 
-  $('#uc').hover(
-    function(){
-      $('#uc').effect( "shake", { direction: "up", times: 4, distance: 10}, 1000 )
-    },
-    function(){
-      $('#uc').complete('shake')
 
-    }
-  )
-  $('#dt').hover(
-    function(){
-      $('#dt').effect( "shake", { direction: "up", times: 4, distance: 10}, 1000 )
-    },
-    function(){
-      $('#dt').complete('shake')
-
-    }
-  )
 
   $('.updog').hover(
     function() {
       $('.dog-overlay').removeClass('hidden')
+      $('#ud').addClass('wobble')
+
     }, function () {
       $('.dog-overlay').addClass('hidden')
+      $('#ud').removeClass('wobble')
+
     }
   )
   $('.daytrippr').hover(
     function() {
       $('.trip-overlay').removeClass('hidden')
+      $('#dt').addClass('lightSpeedIn')
     }, function () {
       $('.trip-overlay').addClass('hidden')
+      $('#dt').removeClass('lightSpeedIn')
+
     }
   )
+
   $('.ubercart').hover(
     function() {
       $('.cart-overlay').removeClass('hidden')
+      $('#uc').addClass('flipInX')
+
     }, function () {
       $('.cart-overlay').addClass('hidden')
+      $('#uc').removeClass('flipInX')
+
     }
   )
 
